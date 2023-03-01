d89df27f924752cc8e21f9424bd6ed0b23af505d
AleksandrSanduliak <89983491+AleksandrSanduliak@users.noreply.github.com>
2023-03-01 21:47:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,29 @@ function likes(names) {
 console.log(likes(['Peter', 'John', 'Dick', 'Daby']))
 
 
+Split the bill
+function splitTheBill(x) {
+let iterable = 0
+const length = Object.keys(x).length
+for(let key in x) iterable += x[key]
+const result = iterable/length
+const arr = Object.entries(x).map(([key,value]) => [key, value-result])
+const obj = {}
+arr.forEach((ev, indx) => obj[ev[0]] = Number(ev[1].toFixed(2)))
+return obj
+}
+
+
+Highest and Lowest
+function highAndLow(numbers){
+let min = numbers[0], max = numbers[0];
+const arr = numbers.split(' ')
+min = Math.min(...arr)
+max = Math.max(...arr)
+return [max, min].join(' ')
+}
+
+
+
+
+
